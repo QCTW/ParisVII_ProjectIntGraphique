1295475d7bcea19a1cd195d6f02b395db8d2cf90
HSIEH Yung-Kun <hsieh@informatique.univ-paris-diderot.fr>
2016-04-02 18:02:13 +0200

    Add content area

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,23 +1,33 @@
 package view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class MainWindow extends BorderPane
 {
 	ToolBar toolBar = new ToolBar();
+	StackPane contentArea = new StackPane();
 	HBox controlBar = new HBox(5);
 
 	public MainWindow()
 	{
 		super();
 		initToolBar(toolBar);
+		initContentArea(contentArea);
 		initControlBar(controlBar);
 		this.setTop(toolBar);
+		this.setCenter(contentArea);
 		this.setBottom(controlBar);
+	}
+
+	private void initContentArea(StackPane s)
+	{
+		s.setPrefSize(Settings.CONTENT_AREA_WIDTH, Settings.CONTENT_AREA_HEIGHT);
 	}
 
 	private void initControlBar(HBox h)
@@ -25,6 +35,7 @@ public class MainWindow extends BorderPane
 		Ball b = new Ball();
 		Cube c = new Cube();
 		h.setPadding(new Insets(Settings.PADDING_IN_BAR));
+		h.alignmentProperty().set(Pos.CENTER);
 		h.getChildren().addAll(b, c);
 	}
 
--- src/view/Settings.java
+++ src/view/Settings.java
@@ -4,4 +4,6 @@ public class Settings
 {
 	public static int NODE_SIZE = 40;
 	public static int PADDING_IN_BAR = 1;
+	public static int CONTENT_AREA_WIDTH = 600;
+	public static int CONTENT_AREA_HEIGHT = 600;
 }
